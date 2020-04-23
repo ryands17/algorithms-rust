bf04b3e3bff3ec5c64ea64c829c71fb6bfd69c87
Ryan Dsouza <dsouza.ryan.11@gmail.com>
2020-04-23 23:28:47 +0530

    implement the anagram check algorithm

---
+++ src/algorithms/anagram.rs
@@ -0,0 +1,24 @@
+use std::collections::HashMap;
+
+pub fn is_anagram(text1: &str, text2: &str) -> bool {
+    let mut text1_map: HashMap<char, u32> = HashMap::new();
+    let mut text2_map: HashMap<char, u32> = HashMap::new();
+
+    for char in text1.chars() {
+        let val = text1_map.entry(char).or_insert(0);
+        *val += 1;
+    }
+
+    for char in text2.chars() {
+        let val = text2_map.entry(char).or_insert(0);
+        *val += 1;
+    }
+
+    for (key, value) in text1_map {
+        if value != *text2_map.get(&key).unwrap_or(&0) {
+            return false;
+        }
+    }
+
+    true
+}
--- src/algorithms/chunks.rs
+++ src/algorithms/chunks.rs
@@ -1,28 +1,27 @@
-pub fn chunk_vec(arr: Vec<i32>, size: Option<u32>) -> Vec<Vec<i32>> {
-  let usize = size.unwrap_or(2);
-  let len = arr.len() as u32;
-  if usize >= len {
-    return vec![arr];
-  }
-
-  let mut chunks = Vec::new();
-
-  let mut i = 0;
-  let mut vec: Vec<i32> = Vec::new();
-  for item in arr {
-    vec.push(item);
-    i += 1;
-
-    if i == usize {
-      i = 0;
-      chunks.push(vec.clone());
-      vec.clear();
+pub fn chunk_vec(arr: Vec<i32>, size: usize) -> Vec<Vec<i32>> {
+    let len = arr.len();
+    if size >= len {
+        return vec![arr];
     }
-  }
 
-  if vec.len() > 0 {
-    chunks.push(vec.clone());
-  }
+    let mut chunks = Vec::new();
 
-  chunks
+    let mut i = 0;
+    let mut vec: Vec<i32> = Vec::new();
+    for item in arr {
+        vec.push(item);
+        i += 1;
+
+        if i == size {
+            i = 0;
+            chunks.push(vec.clone());
+            vec.clear();
+        }
+    }
+
+    if vec.len() > 0 {
+        chunks.push(vec.clone());
+    }
+
+    chunks
 }
